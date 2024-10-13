8013b043198a6e77126dfb0b818eafcb891a8eb8
Pesha375 <onyangojames375@gmail.com>
2024-10-13 20:07:29 +0300

    Updated version of the code with the requirements listed

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{super().__str__()} - EBook, File Size: {self.file_size} MB"
+        return f"EBook: {super().__str__()}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -22,7 +22,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{super().__str__()} - PrintBook, Page Count: {self.page_count}"
+        return f"PrintBook: {super().__str__()}, Page Count: {self.page_count}"
 
 
 class Library:
